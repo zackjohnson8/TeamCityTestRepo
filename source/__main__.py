ccf719fb49f3dda579f1f9d6792229a4686252cb
Zachary Johnson <zackjohnson8@gmail.com>
2021-11-11 12:35:58 -0800

    Added a few lines of code to test out my python abilities again. ;)

---
+++ source/__main__.py
@@ -0,0 +1,10 @@
+import shutil
+import sys
+
+import team_city_math
+
+def main():
+    print(team_city_math.add(2, 3))
+
+if __name__ == "__main__":
+    main()
---
+++ source/team_city_math.py
@@ -0,0 +1,6 @@
+
+
+
+
+def add(val1, val2):
+    return (val1 + val2)
